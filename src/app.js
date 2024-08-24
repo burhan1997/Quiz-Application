dcecc4bb004aa502669c759bd2a15d594318ea5d
burhan1997 <burhanelaldi97@gmail.com>
2024-08-25 01:07:56 +0200

    comment is added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { initEndPage } from './pages/endPage.js'; // Imports the initEndPage fun
 const loadApp = () => {
   quizData.currentQuestionIndex = 0;
   quizData.quizCompleted = false; //quiz starts from the beginning and is not completed when the application loads.
-
+  //
   // Checking that if the quiz is completed
   if (quizData.quizCompleted) {
     initEndPage();
